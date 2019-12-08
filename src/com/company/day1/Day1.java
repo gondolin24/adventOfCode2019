19c88aa8c5a10155aa23cf7a4646323b80a4b2dc
Eduardo Guzman-Diaz <tloz_tlotr@hotmail.com>
2019-12-07 20:39:27 -0500

    day 1 done

--- src/com/company/day1/Day1.java
+++ src/com/company/day1/Day1.java
@@ -2,6 +2,9 @@ package com.company.day1;
 
 import com.company.utils.AdventUtils;
 
+import java.io.Console;
+import java.io.IOException;
+
 public class Day1 {
 
     public Day1(){
@@ -9,15 +12,32 @@ public class Day1 {
     }
 
 
-    public void runMe() throws Exception{
+    public void runMePartOne() throws Exception{
       int sum =   AdventUtils.getFile("D:\\codingStuff\\adventOfCode2019\\src\\com\\company\\day1\\inputs.txt")
                .mapToInt(num-> calcFuelNeeded(Integer.valueOf(num))).sum();
       System.out.println(sum);
     }
 
-    public int calcFuelNeeded(int mass){
+    private int calcFuelNeeded(int mass){
         double floor = Math.floor(mass/3);
         return   ((int)floor) - 2;
+    }
+
+    public void runMePartTwo() throws IOException {
+        int sum =   AdventUtils.getFile("D:\\codingStuff\\adventOfCode2019\\src\\com\\company\\day1\\inputs.txt")
+                .mapToInt(num-> fuelMe(Integer.valueOf(num))).sum();
+        System.out.println(sum);
+    }
+
+    private int fuelMe(int current){
+
+        int fuelNeeded = calcFuelNeeded(current);
+        if(fuelNeeded<=0){
+            return 0;
+        }else{
+        return fuelNeeded + fuelMe(fuelNeeded);
+        }
+
     }
 
 }
