8747a94e155aeb42af99a647df57b57e4e9d2182
Eduardo Guzman-Diaz <tloz_tlotr@hotmail.com>
2019-12-07 20:29:19 -0500

    Update Day1.java

--- src/com/company/day1/Day1.java
+++ src/com/company/day1/Day1.java
@@ -1,4 +1,23 @@
 package com.company.day1;
 
+import com.company.utils.AdventUtils;
+
 public class Day1 {
+
+    public Day1(){
+
+    }
+
+
+    public void runMe() throws Exception{
+      int sum =   AdventUtils.getFile("D:\\codingStuff\\adventOfCode2019\\src\\com\\company\\day1\\inputs.txt")
+               .mapToInt(num-> calcFuelNeeded(Integer.valueOf(num))).sum();
+      System.out.println(sum);
+    }
+
+    public int calcFuelNeeded(int mass){
+        double floor = Math.floor(mass/3);
+        return   ((int)floor) - 2;
+    }
+
 }
